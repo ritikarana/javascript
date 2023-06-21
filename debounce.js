d4a83bcf0f35a361e25c5ca0fae21e0a77eef554
Ritika Rana <ritika.rana07@gmail.com>
2023-06-21 17:18:02 +0100

    Update debounce.js
    Added Debounce Function

--- debounce.js
+++ debounce.js
@@ -1,1 +1,11 @@
 
+
+function debounce(fn , wait){
+    let timerId;
+    return function(...args){
+        if(timerId) clearTimeout(timerId);
+        timerId = setTimeout(()=>{
+           fn(...args)
+        }, wait)
+    }
+}
